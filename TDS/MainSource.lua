3da930eca860f84d0980c3034eb5626174df61e8
Gurty <synergysypher@gmail.com>
2025-01-06 22:01:48 +0700

    Update MainSource.lua

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -2,7 +2,9 @@ local easyBlackList = {
 	676865455,
 	5089488842,
 	1253728146,
-	6135463763
+	6135463763,
+	7733419159,
+	2629007143
 }
 if table.find(easyBlackList, game:GetService("Players").LocalPlayer.UserId) then return end
 if getgenv().StratXLibrary and getgenv().StratXLibrary.Executed then
