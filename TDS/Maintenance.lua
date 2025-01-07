c9bfbecb8c347ee7ec059ef12a3f06d73a3bc59b
Gurty <synergysypher@gmail.com>
2025-01-07 08:44:04 +0700

    Update Maintenance.lua

--- TDS/Maintenance.lua
+++ TDS/Maintenance.lua
@@ -3,8 +3,6 @@ local easyBlackList = {
 	5089488842,
 	1253728146,
 	6135463763,
-	7733419159,
-	2629007143
 }
 if table.find(easyBlackList, game:GetService("Players").LocalPlayer.UserId) then return end
 
